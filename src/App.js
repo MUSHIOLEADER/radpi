42d73c829eea7c5a17e2fe9f43bb27db25bf99f2
MUSHIOLEADER <leadermushio377@gmail.com>
2022-05-12 06:30:16 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import Home from './component/Home';
+import Wwd from './component/Wwd';
+import Navbar from './component/Navbar';
+import Page1 from './component/Page1';
+import Home1 from './component/Home1'
+import Cqf from './component/Cqf';
+import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
+import Footer from './component/Footer';
+import Footer1 from './component/Footer1';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    
+    <div className="app">
+       <Navbar/>
+      <Switch>
+        <Route exact path={'/'}>          
+        <Home/>
+        <Home1/>
+        <Wwd/>
+        <Cqf/>
+        </Route>
+        <Route path={'/page1'}>
+          <Page1/>
+        </Route>
+      </Switch>    
+      <Footer1/>
+      <Footer/>
     </div>
+    </Router>
+
+  
   );
 }
 
